76bf59cf41af40a83150c6cf7f692f736d802b7a
kf3010579 <karl.faust@yahoo.com>
2015-11-10 20:17:05 -0800

    Initial Commit

---
+++ m3u8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  m3u8
+//
+//  Created by Karl Faust on 11/10/15.
+//  Copyright © 2015 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
